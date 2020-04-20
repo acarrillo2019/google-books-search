37f54420d3b774547df596b5dd90fa82aee703ed
acarrillo2019 <alric21@aol.com>
2020-04-19 23:32:18 -0700

    server js

--- server.js
+++ server.js
@@ -4,3 +4,23 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require("./routes");
 const mongoose = require("mongoose");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+// Send every other request to the React app
+// Define any API routes before this runs
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
