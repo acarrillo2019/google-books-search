16d8c790f220dbc5ecf0305197928bb27ee3ca0b
acarrillo2019 <alric21@aol.com>
2020-04-19 23:47:07 -0700

    models

---
+++ models/book.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String },
+  authors: { type: String },
+  description: { type: String },
+  image: { type: String },
+  link: { type: String },
+  google_id: { type: String, required: true, unique: true },
+  date: { type: Date, default: Date.now }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
