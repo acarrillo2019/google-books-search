168df22a88c4cd128f9edc078533cd0a8e71780f
acarrillo2019 <alric21@aol.com>
2020-04-21 19:14:22 -0700

    booksController.js

---
+++ controllers/booksController.js
@@ -0,0 +1,21 @@
+const db = require("../models");
+
+// Defining methods for the booksController
+module.exports = {
+  saveBooks: function(req, res) {
+    db.Book.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findSaved: function(req, res) {
+    db.Book.find({})
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Book.findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
